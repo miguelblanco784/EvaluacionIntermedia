e83b3402905b6f085899f882a41eaa9f1c3ed5bd
miguelblanco784 <miguelrobertoblanco@gmail.com>
2020-08-23 12:13:42 -0600

    Vamos muy bien

--- evaluacionintermedia/src/ApuntesForm.js
+++ evaluacionintermedia/src/ApuntesForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function Apuntes() {
   return (
@@ -37,6 +38,18 @@ export default function Apuntes() {
           </Grid>
 
           <Grid item xs = {12}>
+            <h2>Tags</h2>
+            <ButtonGroup color="primary" aria-label="outlined primary button group">
+              <Button>REACT</Button>
+              <Button>JAVASCRIPT</Button>
+              <Button>FRONT-END</Button>
+              <Button>BACK-END</Button>
+              <Button>HTML</Button>
+              <Button>CSS</Button>
+            </ButtonGroup>
+          </Grid>
+
+          <Grid item xs = {12}>
             <Button variant="contained">Guardar Apunte</Button>
           </Grid>
           
@@ -44,3 +57,4 @@ export default function Apuntes() {
     </React.Fragment>
   );
 }
+
